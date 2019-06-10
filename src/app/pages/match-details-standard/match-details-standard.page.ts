4e43afe941d34634a82fc6309ecca36be71a8ea3
Stevenson <Barry.Stevenson@forcepoint.com>
2019-06-10 21:53:07 +0100

    Updating lateset changes even though theres errors

--- src/app/pages/match-details-standard/match-details-standard.page.ts
+++ src/app/pages/match-details-standard/match-details-standard.page.ts
@@ -6,14 +6,17 @@ import { ActivatedRoute } from '@angular/router';
   selector: 'app-match-details-standard',
   templateUrl: './match-details-standard.page.html',
   styleUrls: ['./match-details-standard.page.scss'],
+
 })
 export class MatchDetailsStandardPage implements OnInit {
   
   public currentMatchDetails: any = {};
   public lineup: Array<any>;
   public tryScorers: Array<any>;
-    
-  constructor(private matchService: MatchService, 
+  public penaltyScorers: Array<any>;
+  public conversionScorers: Array<any>;
+ 
+    constructor(private matchService: MatchService, 
               private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -40,7 +43,7 @@ export class MatchDetailsStandardPage implements OnInit {
         });
         
      /*
-     * Return the Match Lineup 
+     * Return the Try scorers 
      */
      this.matchService.getTryScorers(matchId).get().then(tryScorersSnapshot =>{
         this.tryScorers = [];
@@ -55,10 +58,46 @@ export class MatchDetailsStandardPage implements OnInit {
         /*
         * Sort the scorers into a more readable format
         */
-        this.getSortedScorers(this.tryScorers);
+        //this.getSortedScorers(this.tryScorers);
         });
       
-        
+     /*
+     * Return the Penalty scorers 
+     */
+     this.matchService.getPenaltyScorers(matchId).get().then(penaltyScorersSnapshot =>{
+        this.penaltyScorers = [];
+            penaltyScorersSnapshot.forEach(snap => {
+                this.penaltyScorers.push({
+                    id: snap.id,
+                    name: snap.data().name,
+                });
+                return false;
+            });
+            
+        /*
+        * Sort the scorers into a more readable format
+        */
+        this.getSortedScorers(this.penaltyScorers);
+        }); 
+      
+     /*
+     * Return the Conversion scorers 
+     */
+     this.matchService.getConversionScorers(matchId).get().then(conversionScorersSnapshot =>{
+        this.conversionScorers = [];
+            conversionScorersSnapshot.forEach(snap => {
+                this.conversionScorers.push({
+                    id: snap.id,
+                    name: snap.data().name,
+                });
+                return false;
+            });
+            
+        /*
+        * Sort the scorers into a more readable format
+        */
+        this.getSortedScorers(this.conversionScorers);
+        }); 
   }
     
    /*
@@ -66,19 +105,28 @@ export class MatchDetailsStandardPage implements OnInit {
    * I don't want to see John Smith, John Smith, John Smith - I want to see John Smith (3)
    */
     getSortedScorers(scorers: Array<any>) : Array<any> {
-        
+       
         for (let i = 0; scorers.length ; i++){
-                var numberScored = 1;  
-                var scorerName = scorers[i].name; 
-            for (let j = i + 1; scorers.length - j ; j ++) {
-                    
-                  if(scorers[i].name == scorers[j].name) {
-                      numberScored++;
-                      scorers[i].name = scorerName + " (" + numberScored + ")";
-                      scorers.splice(j,1);
+            var numberScored = 1;
+            
+            //*** Errors here *** when I use .name I get an error on Chrome - not sure what I should be using to get the name value 
+            // It seems to do the logic correctly
+            var scorerName = scorers[i].name;
+            
+            for (let j = i +1 ; scorers.length - j;) {
+                
+                if(scorers[i].name.includes(scorers[j].name)) {
+                 
+                    numberScored++;
+                    scorers[i].name = scorerName + " (" + numberScored + ")";
+                    scorers.splice(j,1);
                     }
-                   }
-               }
+                else{
+                    //Only incerement the j if we haven't spliced the array
+                    j++;
+                    }
+                }
+            }
         return scorers;
         
     } 
--- src/app/services/match/match.service.ts
+++ src/app/services/match/match.service.ts
@@ -13,6 +13,9 @@ export class MatchService {
  public matchesRef: firebase.firestore.CollectionReference;
  public matchLineupRef: firebase.firestore.CollectionReference;
  public tryScorersRef: firebase.firestore.CollectionReference;
+ public penaltyScorersRef: firebase.firestore.CollectionReference;
+ public conversionScorersRef: firebase.firestore.CollectionReference;
+ 
     
   constructor() { 
       this.matchesRef = firebase.firestore().collection('/matches');
@@ -34,6 +37,16 @@ export class MatchService {
   getTryScorers (matchId: string): firebase.firestore.Query {
        this.tryScorersRef = firebase.firestore().collection('/matches/' + matchId + '/tries');
        return this.tryScorersRef.orderBy("name","asc");
+    }
+    
+  getPenaltyScorers (matchId: string): firebase.firestore.Query {
+       this.penaltyScorersRef = firebase.firestore().collection('/matches/' + matchId + '/penalties');
+       return this.penaltyScorersRef.orderBy("name","asc");
+    }
+    
+  getConversionScorers (matchId: string): firebase.firestore.Query {
+       this.conversionScorersRef = firebase.firestore().collection('/matches/' + matchId + '/conversions');
+       return this.conversionScorersRef.orderBy("name","asc");
     }
 
 }
